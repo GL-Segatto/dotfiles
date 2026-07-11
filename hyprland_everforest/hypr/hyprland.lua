1b64af210480aeb3c3ee4018a74a9671c08eb908
gabriel <glimbergersegatto@protonmail.com>
2026-07-11 15:23:41 -0300

    terminal e thunar binds configuradas

--- hyprland_everforest/hypr/hyprland.lua
+++ hyprland_everforest/hypr/hyprland.lua
@@ -122,7 +122,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("awww-daemon")
 	-- Imagem configurada para o daemon
-	hl.exec_cmd("awww img ~/.config/awww-daemon/wallpapers/coffee_cherries.jpg")
+	hl.exec_cmd("awww img ~/.config/awww-daemon/wallpapers/everforest.jpg")
 	-- Adicione aqui mais programas para inciar
 end)
 
@@ -327,17 +327,17 @@ hl.device({
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
-local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
+hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
+local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(
 	mainMod .. " + M",
 	hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
 )
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("thunar"))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 -- bind que abre meu launcher rofi personalizado
-hl.bind(mainMod .. " + D", hl.dsp.exec_cmd("~/.config/rofi/launchers/type-4/launcher.sh || pkill rofi"))
+hl.bind(mainMod .. " + F", hl.dsp.exec_cmd("~/.config/rofi/launchers/type-4/launcher.sh || pkill rofi"))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + T", hl.dsp.layout("togglesplit")) -- dwindle only
 
